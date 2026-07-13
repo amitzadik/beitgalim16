ede81382c37b2f324cacffce1bbe5933433e47ac
amitzadik <amitzadik@gmail.com>
2026-07-13 17:29:12 +0300

    Clear removed practical info component

--- src/components/PracticalInfo/PracticalInfo.tsx
+++ src/components/PracticalInfo/PracticalInfo.tsx
@@ -1,19 +1,3 @@
-import Container from "@/components/ui/Container";
-import styles from "./PracticalInfo.module.css";
-
 export default function PracticalInfo() {
-  return (
-    <section id="info" className={styles.section}>
-      <Container className={styles.container}>
-        <div className={styles.intro}>
-          <h2 className={styles.title}>איך מגיעים ונעים בפסטיבל</h2>
-          <p className={styles.text}>
-            הפסטיבל מתקיים ברחבי שכונת בת גלים, בין הטיילת, המרכז הקהילתי,
-            הבתים הפתוחים ומוקדי התרבות בשכונה. מומלץ להגיע בתחבורה ציבורית
-            ולנוע ברגל בין המוקדים.
-          </p>
-        </div>
-      </Container>
-    </section>
-  );
+  return null;
 }
