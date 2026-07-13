4ebc98f9e998bdb55c1f16cbf648d3fd4d40400c
amitzadik <amitzadik@gmail.com>
2026-07-13 17:24:24 +0300

    Update footer text and remove logo

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import styles from "./Footer.module.css";
 
 export default function Footer() {
@@ -8,19 +7,19 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.inner}>
         <div className={styles.brand}>
-          <Image
-            src="/images/logo2.png"
-            alt="פסטיבל בית גלים ה־16"
-            width={709}
-            height={709}
-            className={styles.logo}
-          />
           <div>
             <p className={styles.brandTitle}>פסטיבל בית גלים ה־16</p>
             <p className={styles.brandSub}>
               שלושה ימים של תרבות, ים ואמנות בשכונת בת גלים
             </p>
           </div>
+        </div>
+
+        <div className={styles.info}>
+          <p>מומלץ להגיע בתחבורה ציבורית ולנוע ברגל בין המוקדים.</p>
+          <p>
+            הסדרי נגישות <span dir="ltr">04-8356356</span>
+          </p>
         </div>
 
         <p className={styles.copyright}>
